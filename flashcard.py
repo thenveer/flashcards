fabc9d2a38a459638f56c37121d6a782eb2da357
muhamed <thenveer81@gmail.com>
2020-01-23 10:43:31 +0530

    flashcard first commit

---
+++ flashcard.py
@@ -0,0 +1,126 @@
+from tkinter import *
+from tkinter import ttk
+import random
+
+
+def get_words(word_file="/home/muhamed/apple/ii.txt"):
+    good_word = []
+    with open(word_file) as f:
+        for word in f:
+            word = word.strip()
+            words =len(word.split())
+            cunt=int(words)
+            #print(words)
+            if int(len(word.split())) < 3:
+                good_word.append(word)
+                
+            
+
+    word =random.choice(good_word)
+   
+    return word
+
+def answer_window(h):
+    global k
+    k=h[1]
+    return (k)
+
+
+def nClick(press):
+    if press==1:
+        global counter
+        counter = 1
+        
+    elif press==2:
+        counter = 2
+    return counter
+
+def first_guess_points(counter):
+    global right
+    global wrong     
+    right=0
+    wrong=0
+    if counter==1:
+        right+=1
+    elif counter==2:        
+        wrong+=1
+    elif counter==3:
+        right=0
+        wrong=0
+        
+    return (counter,right ,wrong)
+def next_guess_points(counter,right,wrong):
+    counter=counter
+    right=right
+    wrong=wrong
+    if counter==1:
+        right+=1
+    elif counter==2:        
+        wrong+=1
+        
+    return (counter,right ,wrong)
+
+
+
+if __name__ == "__main__":
+
+    n=0
+    if n==0:
+        q=first_guess_points(3)
+    while(n<4):
+        
+        window =Tk()
+        window.geometry("500x350")
+   
+        var_1=StringVar()
+        label =Label(window, font="Times 22 bold",textvariable=var_1)
+        label.place(x=15,y=45)
+        word=get_words()
+        h=word.split()
+        
+        var_1.set(h[0])
+
+                
+        var_4=StringVar()
+        label =Label(window, font="Times 22 bold",textvariable=var_4)
+        label.place(x=100,y=160)
+        var_4.set(wrong)
+
+        var_5=StringVar()
+        label =Label(window, font="Times 22 bold",textvariable=var_5)
+        label.place(x=50,y=160)
+        var_5.set(right)
+    
+    
+        but1=Button(window, text="see answer",command=lambda:answer_window(h))
+        but1.place(x = 10,y = 90)
+
+
+        var_2=StringVar()
+        label =Label(window, font="Times 22 bold",textvariable=var_2)
+        label.place(x=50,y=130)
+        q=answer_window(h)
+        var_2.set(q)
+   
+        
+        but2=Button(window, text="am right",command=lambda:nClick(press=1))
+        but2.place(x = 10,y =200)
+
+        
+        but3=Button(window, text="am wrong",command=lambda:nClick(press=2))
+        but3.place(x = 100,y =200)
+    
+
+
+        window.mainloop()
+        n+=1
+       
+        if n==1:
+            c=first_guess_points(counter)
+            
+        else:
+            g=next_guess_points(counter,right,wrong)
+            counter=counter
+            right=g[1]
+            wrong=g[2]
+       
---
+++ test_tk.py
@@ -0,0 +1,64 @@
+from flashcard import get_words,answer_window,nClick,first_guess_points,next_guess_points
+def test_correct_word():
+    with open("/tmp/words.txt","w") as f:
+        for i in ["get the one", "words  t wo", "word three"]:
+            f.write(i+"\n")
+    selected_word = get_words('/tmp/words.txt')
+    assert selected_word == 'word three'
+
+# def test_when_click_answer():
+#     with open("/tmp/words.txt","w") as f:
+#         for i in ["two"]:
+#             f.write(i+"\n")
+#     answer_window = get_words('/tmp/words.txt')
+#     assert answer_window == 'two'
+def test_when_click_answer():
+    h=['kk','oo']
+    assert answer_window(h) == 'oo'
+
+def test_right_button_pressed():
+    press=1
+    assert nClick(press) == 1
+
+def test_wrong__button_pressed():
+    press=2
+    assert nClick(press) == 2
+
+def test_befor_first_guess_point():
+    counter =3
+    assert first_guess_points(counter) == (3,0,0)
+    
+def test_correct_guess_point():
+    counter =1
+    assert first_guess_points(counter) == (1,1,0)
+def test_wrong_guess_point():
+    counter =2
+    assert first_guess_points(counter) == (2,0,1)
+
+def test_next_correct_point():
+    counter=1
+    right=0
+    wrong=0
+    assert next_guess_points(counter,right,wrong) == (1,1,0)
+
+def test_next_wrong_point():
+    counter=2
+    right=0
+    wrong=0
+    assert next_guess_points(counter,right,wrong) == (2,0,1)
+
+def test_again_correct_point():
+    counter=1
+    right=2
+    wrong=0
+    assert next_guess_points(counter,right,wrong) == (1,3,0)
+
+def test_again_wrong_point():
+    counter=2
+    right=1
+    wrong=1
+    assert next_guess_points(counter,right,wrong) == (2,1,2)
+
+
+
+
